27bc9ea5378c7c1883eff781347dbaad49195603
Saif Rakib Hasan <saifrakib99@gmail.com>
2021-10-27 02:09:23 +0600

    Car mechanics project

---
+++ src/Pages/Booking/Booking/Booking.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+const Booking = () => {
+    const { serviceId } = useParams();
+    const [service,setService] =useState({});
+
+    useEffect( () => {
+        fetch(`https://afternoon-springs-85204.herokuapp.com/services${serviceId}`)
+        .then(res => res.json())
+        .then(data => setService(data)); 
+    },[])
+    return (
+        <div>
+            <h2>Details of :{service.name}</h2>
+            <h2>this is booking: {serviceId}</h2>
+        </div>
+    );
+};
+
+export default Booking;
